027188a8f31df76ed798e6bc2b7abba33ccd3a10
kt-321 <kt.shzk247@gmail.com>
2020-11-24 09:28:33 +0900

    #pointer ポインタ変数演習

--- main.go
+++ main.go
@@ -2,6 +2,60 @@ package main
 
 import "fmt"
 
+type Hoge struct {
+	Label string
+	Index int
+	Name string
+}
+
 func main() {
-	fmt.Print("test")
+	name := "hoge"
+	fmt.Println(&name)
+
+	var hoge *Hoge
+
+	//Hogeへのポインタである*Hoge型の値を入れている
+	hoge = &Hoge{
+		Label: "test",
+		Index: 0,
+		Name: "testだよ",
+	}
+
+	fmt.Println(hoge)
+	fmt.Println(*hoge)
+
+	//++++++++++++++++++++++++++++++++
+
+	hoge2 := Hoge{
+		Label: "test2",
+		Index: 1,
+		Name: "test2です。",
+	}
+
+	fmt.Println(hoge2)
+
+	h := hoge2
+	h.Label = "change"
+	fmt.Println(h)
+	fmt.Println(hoge2)
+
+	h2 := &hoge2
+	h2.Label = "change"
+	fmt.Println(*h2)
+	fmt.Println(hoge2)
+
+
+	//++++++++++++++++++++++++++++++++++
+	//hoge3はポインタ変数
+	var hoge3 *int
+
+	fmt.Println(hoge3)
+
+	sample := 2
+	hoge3 = &sample
+
+	//アドレスモード
+	fmt.Println(hoge3)
+	//間接参照モード
+	fmt.Println(*hoge3)
 }
