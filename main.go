9759477241967eda72e1085858ca0336bb651b5c
kt-321 <kt.shzk247@gmail.com>
2020-11-26 11:27:46 +0900

    Stringers演習

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type IPAddr [4]byte
+
+func (i IPAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", i[0], i[1], i[2], i[3])
+}
+
 func main() {
-	fmt.Print("test")
+	hosts := map[string]IPAddr{
+		"loopback": {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+
+	//mapをrangeでイテレーションすると実行するたびに異なる結果。乱択アルゴリズム
+	for _, ip := range hosts{
+		fmt.Printf("%v\n", ip)
+	}
 }
