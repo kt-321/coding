b3dc8e700ed69ead4ab1ee702be1b256d9c0253f
kt-321 <kt.shzk247@gmail.com>
2020-11-27 09:54:11 +0900

    mathパッケージ・fmtパッケージ

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("test")
+	//浮動小数点数
+	//var a float64 = 100
+	//fmt.Printf("%T: %v", a, a)
+
+	//円周率
+	fmt.Println(math.Pi)
+
+	//絶対値
+	test := math.Abs(-100.333)
+	fmt.Println(test)
+
+	//累乗
+	fmt.Println(math.Pow(2, 3))
+
+	//平方根(square root)
+	fmt.Println(math.Sqrt(81))
+
+	var a bool = true
+	fmt.Printf("%t", a)
 }
