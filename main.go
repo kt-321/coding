4c6d953bce687ef3eca6b0265fd16a6307b5278b
kt-321 <kt.shzk247@gmail.com>
2020-11-29 12:15:55 +0900

    #json jsonのデコード演習

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type response1 struct {
@@ -36,4 +37,31 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println(string(testMarshaled))
+
+	byt := []byte(`{"num":6.13, "strs":["a", "b"]}`)
+
+	var dat map[string]interface{}
+
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%T: %v: %T: %v: %T: %v\n", dat, dat, dat["num"], dat["num"], dat["strs"], dat["strs"])
+
+	num := dat["num"].(float64)
+	fmt.Printf("%T: %v:\n", num, num)
+
+	strs := dat["strs"].([]interface{})
+	fmt.Printf("%T: %v:\n", strs, strs)
+	str1 := strs[0].(string)
+	fmt.Printf("%T: %v:\n", str1, str1)
+
+	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	fmt.Printf("%T: %v:\n", str, str)
+	res := &response2{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Printf("%T: %v: %T: %v\n", res, res, res.Fruits[0], res.Fruits[0])
+
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lettuce" :7}
+	enc.Encode(d)
 }
