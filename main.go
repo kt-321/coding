e66ae2f2bf11ee1c309237a0557696c883261c57
kt-321 <kt.shzk247@gmail.com>
2020-12-08 08:59:38 +0900

    ATouofGoのslices

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	s := make([][]uint8, dy)
+
+	for y:=0; y<dy; y++ {
+		s2 := make([]uint8, dx)
+		for x:=0; x<dx; x++{
+			s2[x] = uint8((x+y)/2)
+		}
+		s[y] = s2
+	}
+	return s
+}
 
 func main() {
-	fmt.Print("test")
+	pic.Show(Pic)
 }
