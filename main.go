d0b9874032afaea6b453095532ce95369a7b325e
kt-321 <kt.shzk247@gmail.com>
2020-12-13 11:37:33 +0900

    exercise-readers

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"golang.org/x/tour/reader"
+)
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+
+func (m MyReader) Read(b []byte) (int, error) {
+	for i:= range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
 
 func main() {
-	fmt.Print("test")
+	//r := strings.NewReader("Hello")
+	//fmt.Printf("%T: %v\n", r, r)
+	//
+	//b := make([]byte, 8)
+	//n, _ := r.Read(b)
+	//fmt.Printf("%T: %v", n, n)
+
+	//fmt.Printf("%T: %v", MyReader{}, MyReader{})
+
+	reader.Validate(MyReader{})
 }
