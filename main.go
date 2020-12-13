31b50a8d7173ecb0cc14895f4d33a1ed5d114d83
kt-321 <kt.shzk247@gmail.com>
2020-12-13 12:48:33 +0900

    exercise rot13Reader

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (rot *rot13Reader) Read(b []byte) (int, error){
+	n, err := rot.r.Read(b) //書き方
+	for i, v := range b {
+		switch {
+		case v >= 'A' && v < 'N', v >= 'a' && v < 'n':
+			b[i] += 13
+		case v >= 'N' && v <= 'Z', v >= 'n' && v <= 'z':
+			b[i] -= 13
+		}
+	}
+	return n, err
+}
 
 func main() {
-	fmt.Print("test")
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	fmt.Printf("%T: %v\n", s, s)
+
+	r := rot13Reader{s}
+	fmt.Printf("%T: %v\n", r, r)
+
+	io.Copy(os.Stdout, &r) //TODO 復習
 }
