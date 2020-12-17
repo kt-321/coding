680356e16eec82cca713958f5733a303f9ff99f8
kt-321 <kt.shzk247@gmail.com>
2020-12-17 09:24:13 +0900

    ATourofGoのsync.Mutex

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// SafeCounter is safe to use concurrently.
+type SafeCounter struct {
+	mu sync.Mutex
+	v  map[string]int
+}
+
+// Inc increments the counter for the given key.
+func (c *SafeCounter) Inc(key string) {
+	fmt.Println("++++++++++++")
+	fmt.Println("Inc!!!")
+	c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+
+	fmt.Println(c.v)
+
+	c.v[key]++
+
+
+	c.mu.Unlock()
+}
+
+// Value returns the current value of the counter for the given key.
+func (c *SafeCounter) Value(key string) int {
+	fmt.Println("++++++++++++")
+	fmt.Println("Value!!!")
+
+	//c.mu.Lock()
+	// Lock so only one goroutine at a time can access the map c.v.
+	//defer c.mu.Unlock()
+	return c.v[key]
+}
 
 func main() {
-	fmt.Print("test")
+	c := SafeCounter{v: make(map[string]int)}
+	for i := 0; i < 1000; i++ {
+		go c.Inc("somekey")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("somekey"))
 }
