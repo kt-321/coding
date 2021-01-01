8f6977dd5b59d44483dcaeb5c87542b6d897b8ef
kt-321 <kt.shzk247@gmail.com>
2021-01-01 21:11:37 +0900

    #12.31

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func Solution(A []int) int {
+	var result int
+
+	loop:
+		for i:=1; i<1000001; i ++ {
+			fmt.Printf("i: %v!!!\n", i)
+			for n, v := range A {
+				// 値が一致する時はループ抜ける
+				if i == v {
+					fmt.Printf("%vは値一致\n", v)
+					break
+				} else if n == len(A) - 1{
+					// 最終ループで値が一致しない時は多重ループを停止
+					result = i
+
+					break loop
+				}
+				fmt.Println(i, v)
+				fmt.Println("最終ループでなく値が一致しない")
+			}
+		}
+
+
+	return result
+}
 
 func main() {
-	fmt.Print("test")
+	s := []int {-1, -10, 1, 2, 100, 3, 4, 5}
+
+	a := Solution(s)
+	fmt.Println(a)
 }
